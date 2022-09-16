af7331d529c7e6ee64a15ae50bb4203e8a16fead
iknow6aint <saintagbukor@gmail.com>
2022-09-16 09:24:33 -0300

    component start

---
+++ src/components/Contacts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Contacts = () => {
+  return (
+    <div>
+        <h4> Jonh Doe</h4>
+        <ul>
+            <li>Email : iknowsaint@gmail.com</li>
+            <li>Phone: 08066658213</li>
+        </ul>
+    </div>
+  )
+}
