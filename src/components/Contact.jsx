0a40fdce5277db5c59c6b1563506b923a194f882
iknow6aint <saintagbukor@gmail.com>
2022-09-18 19:32:30 -0300

    delete refactor

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,13 @@
+import { getValue } from '@testing-library/user-event/dist/utils';
 import React, { Component } from 'react';
+import { Consumer } from '../context';
 // import PropTypes from 'prop-types';
+
 
 
 class Contact extends Component {
     state = {
-        showContactInfo: true
+        showContactInfo: false
     };
 
     onShowClick = e => {
@@ -13,37 +16,46 @@ class Contact extends Component {
                 !this.state.showContactInfo
         })
     }
-    onDeleteClick = e => {
-        this.props.deleteClickHandler();
+    onDeleteClick = (id, dispatch) => {
+        dispatch({ type: 'DELETE_CONTACT', payload: id })
     }
     render() {
-        const { name, email, phone } = this.props.contact;
+        const { name, email, phone, id } = this.props.contact;
         const { showContactInfo } = this.state;
 
         return (
-            <div>
-                <div className='card-body m3-3'>
-                    <h4>{name}
-                        <i onClick={this.onShowClick}
-                            className='fas fa-sort-down'
-                            style={{ cursor: 'pointer' }}
-                        />
-                        <i className="fas fa-times" style={
-                            {
-                                cursor: 'pointer', float:
-                                    'right', color: 'red'
-                            }}
-                            onClick={this.onDeleteClick}
-                        />
-                    </h4>
-                    {showContactInfo ? (
-                        <ul className='list-group'>
-                            <li className='list-group-item'>
-                                email:{email}</li>
-                            <li className='list-group-item'>phone:{phone}</li>
-                        </ul>) : null}
-                </div>
-            </div>
+
+            <Consumer>
+                {value => {
+                    const { dispatch } = value
+                    return (
+                        <div>
+                            <div className='card-body m3-3'>
+                                <h4>{name}
+                                    <i onClick={this.onShowClick}
+                                        className='fas fa-sort-down'
+                                        style={{ cursor: 'pointer' }}
+                                    />
+                                    <i className="fas fa-times" style={
+                                        {
+                                            cursor: 'pointer', float:
+                                                'right', color: 'red'
+                                        }}
+                                        onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                                    />
+                                </h4>
+                                {showContactInfo ? (
+                                    <ul className='list-group'>
+                                        <li className='list-group-item'>
+                                            email:{email}</li>
+                                        <li className='list-group-item'>phone:{phone}</li>
+                                    </ul>) : null}
+                            </div>
+                        </div>
+                    )
+                }}
+            </Consumer>
+
         )
     }
 
